241ed1ce453c2a7f69fa97002db531934b66caad
artiwok <artemolga@ukr.net>
2020-10-24 19:43:58 +0300

    Add project files.

---
+++ DynamicDiamond/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicDiamond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DynamicDiamond
+            //   *
+            //  ***
+            // *****
+            //  ***
+            //   *    
+
+            Console.Write("Enter height: ");
+
+            int height = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= height; i++)
+            {
+                for (int j = height; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                for (int j = 1; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+            for (int i = 1; i < height; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = height; j > i; j--)
+                {
+                    Console.Write("*");
+                }
+
+                for (int j = 1; j < height - i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
